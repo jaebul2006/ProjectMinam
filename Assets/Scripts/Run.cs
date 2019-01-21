83725903749f6e66bbb68d7d4f2ec0a21b0db72c
jdh <jaebul2006@naver.com>
2019-01-21 18:53:40 +0900

    first minam

---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour {
+
+    bool game_pause = false;
+
+	void Start ()
+    {
+	    	
+	}
+
+    int face_shape = 1;
+    int face_bottom = 1;
+    int eye = 1;
+    int nose = 1;
+    int hair = 1;
+	
+    public void ChangeFace()
+    {
+        
+    }
+
+	void Update ()
+    {
+		if(game_pause == false)
+        {
+            GameRun();
+        }
+	}
+
+    private void GameRun()
+    {
+        GetGold();
+    }
+
+    private void GetGold()
+    {
+
+    }
+
+}
